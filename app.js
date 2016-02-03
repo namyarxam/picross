45ccb8234429ea0fffb2d61e9c491827cb9e1071
Max Rayman <mxray333@gmail.com>
2016-02-02 22:14:07 -0500

    added function to calculate cell clusters for axis labeling

--- app.js
+++ app.js
@@ -9,5 +9,29 @@ function randomBoard() {
 
   var board = boardBinary.split('');
 
+  for (var i = 0; i<board.length; i++) {
+    board[i] = parseInt(board[i]);
+  }
+
   return board;
+}
+
+//calculates the clusters of '1' in a binary string (axis labels)
+function axis(arr) {
+  var label = [];
+  var x = 0;
+
+  for (var i=0; i<arr.length; i++) {
+    if(arr[i] === 1) {
+        if (!label[x]) {
+          label[x] = 0;
+        }
+        label[x]++;
+    } else {
+      x++;
+    }
+  }
+
+  label=label.filter(Number);
+  return label;
 }
